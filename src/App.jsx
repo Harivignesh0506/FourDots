b4b0cea94e29ea1265e216e744ae5dcec4d7385f
Harivignesh0506 <harivignesh0506@gmail.com>
2024-12-17 22:45:45 +0530

    file uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import LiveStreamingBanner from './Components/Live'
+import Main from './Components/Price';
 
 function App() {
   return (
     <div className="App">
     <LiveStreamingBanner />
+    <Main />
     </div>
   );
 }
---
+++ src/Components/Price.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='relative top-[1500px] w-full h-[500px] border border-black bg-black text-white'>
+      <h1 className='text-2xl flex justify-center font-bold p-1 relative top-3' > Subscription Priced ?</h1>
+      <Price />
+      <h2 className='relative top-[100px] text-2xl flex justify-center '>*Stream unlimited matches in Daily, Monthly, Yearly Plans</h2>
+      <p className="border border-white w-[250px] h-[50px] flex justify-center items-center rounded cursor-pointer bg-sky-700 border-none hover:bg-white hover:text-black relative left-[650px] top-[150px] ">PURCHASE LIVE STREAM</p>
+    </div>
+  );
+};
+
+const Cards = ({ data }) => {
+  const { title, price } = data ?? {}; // Object destructuring
+  return (
+    <div className='border bg-slate-400 w-[220px] h-[150px] rounded flex flex-col justify-center items-center relative top-[70px] border-none'>
+      <h1>{title}</h1>
+      <h1>{price}</h1>
+    </div>
+  );
+};
+
+const Price = () => {
+  const card = {
+    title: "Per Mach",
+    price: "₹ 269",
+  };
+  const card1 = {
+    title: "One Day plan",
+    price: "₹ 1299",
+  };
+  const card2 = {
+    title: "Monthly Plan",
+    price: "₹ 4499",
+  };
+  const card3 = {
+    title: "Monthly Premium ",
+    price: "₹ 6000",
+  };
+  const card4 = {
+    title: "Yearly Plan",
+    price: "₹ 11999",
+  };
+  const card5 = {
+    title: "Per Mach",
+    price: "₹ 14999",
+  };
+
+  return (
+    <div className="flex gap-3 justify-center text-2xl font-bold items-center ">
+      <Cards data={card} />
+      <Cards data={card1} />
+      <Cards data={card2} />
+      <Cards data={card3}  />
+      <Cards data={card4} />
+      <Cards data={card5} />
+    </div>
+  );
+};
+
+export default Main;
